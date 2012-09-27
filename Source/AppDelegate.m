166fc085bdcd975b42eb29ed906600d05290d64f
Frédéric Sagnes <fred@teapotapps.com>
2012-09-27 18:18:30 +0200

    Add icons and splash screens

--- Source/AppDelegate.m
+++ Source/AppDelegate.m
@@ -6,8 +6,16 @@
 //  Copyright (c) 2012 teapot apps. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
+
 #import "AppDelegate.h"
 #import "ViewController.h"
+
+@interface AppDelegate ()
+
+- (void)dumpSplashImage;
+
+@end
 
 @implementation AppDelegate
 
@@ -18,8 +26,22 @@
     window.rootViewController = rootViewController;
     self.window = window;
     [window makeKeyAndVisible];
+//    [self performSelector:@selector(dumpSplashImage) withObject:nil afterDelay:0.5];
 
     return YES;
+}
+
+- (void)dumpSplashImage {
+    UIWindow *window = self.window;
+    NSString *fileName = window.screen.scale > 1.0f ? (window.bounds.size.height == 568 ? @"Default-568h@2x.png" : @"Default@2x.png") : @"Default.png";
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:fileName];
+
+    UIGraphicsBeginImageContextWithOptions(window.bounds.size, YES, 0.0f);
+    [window.layer renderInContext:UIGraphicsGetCurrentContext()];
+    NSLog(@"Writing splash screen to path %@", filePath);
+    [UIImagePNGRepresentation(UIGraphicsGetImageFromCurrentImageContext()) writeToFile:filePath atomically:YES];
+    UIGraphicsEndImageContext();
 }
 
 @end
